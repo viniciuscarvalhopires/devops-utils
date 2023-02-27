72058e4f62d5ae113a577e339ba9748ea5d52164
Vinicius Carvalho Pires <vinicpires03@gmail.com>
2023-02-26 21:53:13 -0300

    Update script.sh
    Add: "export KUBECONFIG=Kubeconfig"

--- script.sh
+++ script.sh
@@ -4,6 +4,8 @@
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+export KUBECONFIG=Kubeconfig
+
 # Sobreescrever o html do nginx com as informações dos namespaces
 echo "<html><body><h1>Lista de namespaces: </h1><ul>" > /usr/share/nginx/html/index.html
 # For loop utilizado para pegar o nome do namespace em cada item retornado pelo kubectl get namespaces
