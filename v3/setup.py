9aa1a9fbe37a0f3064365776c3420895770e4616
viniciuscarvalhopires <vinicpires03@gmail.com>
2023-03-30 13:10:52 -0300

    update: v3

--- v3/setup.py
+++ v3/setup.py
@@ -1,13 +1,11 @@
 import subprocess as sp
 import os
 
-sp.run(["kubectl", "cluster-info"])
-
 sp.run(["spawn-fcgi", "-s", "/var/run/fcgiwrap.socket", "/usr/bin/fcgiwrap"])
 
 os.chmod("/var/run/fcgiwrap.socket", 0o777) 
 
-# UID e GID em /etc/passwd ou id -g <user> e id -u <user>
+# UID e GID em /etc/passwd ou id -g <user> e id -u <user>, neste caso, equivale ao usuário www-data
 os.chown("/usr/lib/cgi-bin/get-namespaces", 33, 33)
 
 os.chmod("/usr/lib/cgi-bin/get-namespaces", 0o755)
