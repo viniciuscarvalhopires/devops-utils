27ec48a5bf6dac49c4d26f1bf9cd7aab2b3313aa
viniciuscarvalhopires <vinicpires03@gmail.com>
2023-04-05 13:43:43 -0300

    Fast API V2

--- fastapi/README.md
+++ fastapi/README.md
@@ -1,3 +1,44 @@
 ## vinicpires/get-namespaces:v3
 
 A imagem *vinicpires/get-namespaces:fastapi* utiliza scripts em python para a automação da coleta de namespaces e não possui arquivo de configuração de cluster inserido, portanto, ao aplicar os manifestos da pasta **manifestos**, certifique-se de que a ClusterRole, ClusterRoleBinding e ServiceAccount foram criadas corretamente.
+
+| :warning: O endpoint **/delete** recebe uma requisição POST através do form html.
+|-----------------------------------------|
+
+## Documentação da API
+
+#### Home
+
+```http
+  GET /
+```
+
+#### Lista todos os namespaces
+
+```http
+  GET /namespaces
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `id`      | `string` | **Obrigatório**. O ID do item que você quer |
+
+#### Deletar namespace
+
+```http
+  POST /delete
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `namespace`      | `string` | **Obrigatório**. Namespace a ser deletado |
+
+#### Deletar namespace
+
+```http
+  POST /create
+```
+
+| Parâmetro   | Tipo       | Descrição                                   |
+| :---------- | :--------- | :------------------------------------------ |
+| `namespace`      | `string` | **Obrigatório**. Namespace a ser criado |
--- fastapi/fastapi.py
+++ fastapi/fastapi.py
@@ -1,33 +1,66 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 from kubernetes import client, config
+import json
 
 app = FastAPI()
 
+config.load_incluster_config()
+
+api = client.CoreV1Api()
+
+class Namespace(BaseModel):
+    name: str
+
 @app.get("/")
-async def get_namespaces():
+async def index():
+    html_content = '''<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form action="/delete" method="POST">
+            <h2>Deletar namespace</h2>
+            <label for="namespace">Nome do namespace</label>
+            <input type="text" name="namespace" id="namespace" required>
+            <input type="submit" value="Deletar">
+        </form>
 
-    html_content = '''
-        <html>
-        <head><meta http-equiv="refresh" content="3"></head>
-        <body>
-        <h2>Desafio com script python</h2>
-        <ul>
-        '''
-
-    # Estabelece a conexão com o cluster
-    config.load_incluster_config()
-
-    api = client.CoreV1Api()
-
-    namespaces = api.list_namespace().items
-
-    for namespace in namespaces:
-        html_content += f"<li>{namespace.metadata.name}</li>"   
-
-    html_content += ''' 
-        </ul>
-        </body>
-        </html>
-        '''   
+        <form action="/create" method="POST">
+            <h2>Criar namespace</h2>
+            <label for="namespace">Nome do namespace</label>
+            <input type="text" name="namespace" id="namespace" required>
+            <input type="submit" value="Criar">
+        </form>
+        <form action="/namespaces" method="GET">
+        <label for="list">Lista de namespaces</label>
+        <input type="submit" id="list" value="Listar namespaces">
+        </form>
+    </body>
+    </html>'''
     return HTMLResponse(content=html_content, status_code=200)
+
+@app.get("/namespaces")
+async def get_namespaces():
+    namespaces = api.list_namespace()
+    namespace_json = client.ApiClient().sanitize_for_serialization(namespaces)
+    return JSONResponse(content=namespace_json, status_code=200)
+
+
+@app.post("/delete")
+async def delete_namespace(namespace: str = Form(...)):
+    name = name = jsonable_encoder(namespace)
+    api.delete_namespace(name=name)
+
+    return f"O namespace {name} foi deletado com sucesso, acesse o endpoint /namespaces para verificar"
+
+
+@app.post("/create")
+async def create_namespace(namespace: str = Form(...)):
+    name = jsonable_encoder(namespace)
+    api.create_namespace(body=client.V1Namespace(
+        metadata=client.V1ObjectMeta(name=name)))
+
+    return f"O namespace {name} foi criado com sucesso, acesse o endpoint /namespaces para verificar"
